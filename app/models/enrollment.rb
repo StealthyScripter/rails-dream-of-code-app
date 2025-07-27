af7a80918e535ec2c4dc1c3fe56b70c5a24e2542
StealthyScripter <wendotbrian@gmail.com>
2025-07-27 00:01:32 -0400

    Lesson 2: Data modeling and rails queris, Define class project, design ERD with Associations for invemtory tracking

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,5 @@ class Enrollment < ApplicationRecord
   belongs_to :course
   belongs_to :student
   has_many :mentor_enrollment_assignments
+  has_many :mentors, through: :mentor_enrollment_assignments
 end
